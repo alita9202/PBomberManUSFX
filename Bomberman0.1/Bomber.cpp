38e6a5e8651b6ea3bb3af1974bc4903581ef07d8
Alejandra Terceros Arcani <alita4774@gmail.com>
2021-09-12 10:55:22 -0400

    clases

--- Bomberman0.1/Bomber.cpp
+++ Bomberman0.1/Bomber.cpp
@@ -1,1 +1,14 @@
 #include "Bomber.h"
+class Bomber
+{
+private:
+	int anchoAvatar;
+	int altoAvatar;
+public:
+	int getAnchoAvatar() { return anchoAvatar; }
+	void setAnchoAvatar(int _anchoAvatar) { anchoAvatar = _anchoAvatar;}
+
+	int getAltoAvatar() { return altoAvatar; }
+	void setAltoAvatar(int _altoAvatar) { anchoAvatar = _altoAvatar;}
+
+};
