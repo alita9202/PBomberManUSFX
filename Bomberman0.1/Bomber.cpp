652852d80cae96ddb623bebf2457aa6fd9a6c59f
Alejandra Terceros Arcani <alita4774@gmail.com>
2021-09-12 12:25:13 -0400

    Modificacion de Clases

--- Bomberman0.1/Bomber.cpp
+++ Bomberman0.1/Bomber.cpp
@@ -1,14 +1,9 @@
 #include "Bomber.h"
-class Bomber
+
+void Bomber::Morir()
 {
-private:
-	int anchoAvatar;
-	int altoAvatar;
-public:
-	int getAnchoAvatar() { return anchoAvatar; }
-	void setAnchoAvatar(int _anchoAvatar) { anchoAvatar = _anchoAvatar;}
 
-	int getAltoAvatar() { return altoAvatar; }
-	void setAltoAvatar(int _altoAvatar) { anchoAvatar = _altoAvatar;}
+}
+void Bomber::Correr() {
 
-};
+}
--- Bomberman0.1/Bomber.h
+++ Bomberman0.1/Bomber.h
@@ -1,5 +1,20 @@
 #pragma once
 class Bomber
 {
+	private:
+		int anchoAvatar;
+		int altoAvatar;
+
+	public:
+		int getAnchoAvatar() { return anchoAvatar; }
+		void setAnchoAvatar(int _anchoAvatar) { anchoAvatar = _anchoAvatar; }
+
+		int getAltoAvatar() { return altoAvatar; }
+		void setAltoAvatar(int _altoAvatar) { altoAvatar = _altoAvatar; }
+
+		void Morir();
+		void Correr();
+
+
 };
 
