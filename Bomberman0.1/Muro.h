1e67d8f91cc0f06ee4b2badbe7caf682c593f7c9
Alejandra Terceros Arcani <alita4774@gmail.com>
2021-09-12 12:36:48 -0400

    modificacio de Muro

--- Bomberman0.1/Muro.h
+++ Bomberman0.1/Muro.h
@@ -1,5 +1,15 @@
 #pragma once
 class Muro
 {
+private:
+	int muroIndestructible;
+	int muroDestructible;
+
+public:
+	int getMuroIndestructible() { return muroIndestructible; }
+	void setMuroIndestructible(int _muroIndestructible) { muroIndestructible = _muroIndestructible; }
+
+	int getMuroDestructible() { return muroDestructible; }
+	void setMuroDestructible (int _muroDestructible) { muroDestructible = _muroDestructible; }
 };
 
